b6c7c5f05cbb6ca397b576159ed8831ddd804550
Vitor <torquatovitor68@gmail.com>
2024-03-19 09:00:06 +0100

    Loging page finish

--- src/pages/Singin/index.jsx
+++ src/pages/Singin/index.jsx
@@ -1,12 +1,43 @@
-import { Container } from './styles'
+import {FiMail, FiLock} from 'react-icons/fi';
+
+import { Input } from '../../components/Input';
+import {Button} from '../../components/button'
 
 
 
+import { Container, Form, Background } from './styles';
 export function SignIn(){
     return(
         <Container>
+
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salver e gerenciar seus links úteis</p>
+
+                <h2>faça seu login</h2>
+
+                <Input 
+                    placeholder="E-mail"
+                    type="text"
+                       icon={FiMail} 
+                />
+
+                <Input 
+                    placeholder="Password"
+                    type="password"
+                       icon={FiLock} 
+                />
+
+                <Button title="Login"/>
+
+                <a href="#">
+                    Create note
+                </a>
             
 
+            </Form>
+
+            <Background/>
 
         </Container>
     )
