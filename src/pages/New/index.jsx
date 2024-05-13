8c6a7cdb2e883dd3af02481ef2f9b16aa1a11d0b
Vitor <torquatovitor68@gmail.com>
2024-05-13 18:29:10 +0200

    creating notes

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { Container, Form } from "./styles";
 import { Link } from 'react-router-dom'
 
+import { useNavigate } from "react-router-dom";
+
+import { api } from "../../Services/api";
 
 import { Header } from '../../components/header'
 import { Input } from '../../components/Input'
@@ -11,6 +15,55 @@ import { Button } from "../../components/button";
 
 
 export function New() {
+
+    const [title,setTitle] = useState("");
+    const [description,setDescription] = useState("");
+
+
+    const [links,setLinks] = useState([]);
+    const [newLink,setNewLink] = useState("");
+
+    const [tags,setTags] = useState([]);
+    const [newTag,setNewTag] = useState("");
+
+    const navigate = useNavigate();
+
+    function handleAddLink(){
+        setLinks(prevState => [...prevState, newLink] );
+        setNewLink("");
+       
+    }
+
+    function handleRemoveLink(deleted){
+        setLinks(prevState => prevState.filter(link => link !== deleted));
+    }
+
+    function handleAddTag(){
+        setTags(prevState => [...prevState,newTag]);
+        setNewTag("")
+
+        
+
+    }
+
+    function handleRemoveTag(deleted){
+        setTags(prevState => prevState.filter(tag => tag !== deleted));
+    }
+
+
+    async function handleNewNote(){
+
+        await api.post("/notes" , {
+            title,
+            description,
+            tags,
+            links
+        })
+
+        alert("Nota cadastrada com sucesso!");
+        navigate("/");
+    }
+
     return (
         <Container>
             <Header />
@@ -24,22 +77,64 @@ export function New() {
 
                     <Input
                         placeholder="Título"
+                        onChange={e => setTitle(e.target.value)}
                     />
-                    <TextArea placeholder="Observações" />
+                    <TextArea
+                     placeholder="Observações"
+                     onChange={e => setDescription(e.target.value)}
+                     />
 
                     <Section title="Links úteis">
-                        <NoteItem value="https://rocketseat.com.br" />
-
-                        <NoteItem isNew placeholder="Novo link" />
+                        {
+                            links.map((link , index) => (
+                                
+                                <NoteItem
+                                key={String(index)}
+                                value={link}
+                                onClick={( ) => handleRemoveLink(link)} 
+                                 />
+                            ))
+                        
+                        }    
+                        <NoteItem
+                         isnew
+                         placeholder="Novo item"
+                         value={newLink}
+                         onChange={e => setNewLink(e.target.value)}
+                         onClick={handleAddLink} 
+                          />
+                         
+                      
 
                     </Section>
 
                     <Section title="Marcadores">
                         <div className="tags">
-                            <NoteItem value="React" />
-                            <NoteItem isNew placeholder="Nova tag" />
+                            
+                        {
+                            tags.map((tag , index) => (
+                            
+                                <NoteItem
+                                key={String(index)}
+                                value={tag}
+                                onClick={( ) => handleRemoveTag(tag)} 
+                                 />
+                            ))
+                        
+                        } 
+                            
+                            <NoteItem
+                                isnew
+                                placeholder="Nova tag"
+                                onChange={e => setNewTag(e.target.value)}
+                                value={newTag}
+                                onClick={handleAddTag}
+                              />
                         </div>
-                        <Button title="Salvar" />
+                        <Button
+                         title="Salvar"
+                         onClick={handleNewNote} 
+                        />
                     </Section>
                 </Form>
 
